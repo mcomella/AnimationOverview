aace39b997600a19ff2bce0c21570d2ddb39d68c
Michael Comella <michael.l.comella@gmail.com>
2018-02-14 11:37:04 -0800

    Write thoughts on AnimationSetActivity.

--- app/src/main/java/xyz/mcomella/animationoverview/view/ViewAnimationSetActivity.kt
+++ app/src/main/java/xyz/mcomella/animationoverview/view/ViewAnimationSetActivity.kt
@@ -13,18 +13,37 @@ import xyz.mcomella.animationoverview.R
 /** Percentage of the banner animation that will pass before the text should start. */
 private const val TEXT_START_PERCENT = 0.70
 
+/**
+ * A "View Animation" using the [AnimationSet] API to coordinate separate animations.
+ *
+ * Learnings:
+ * - Must define the animation over five files (3x animation, 1 layout, 1 code) since you can't
+ * define one animation file for multiple views coordinating. These files require context switching
+ * and make it hard to know what the animation does without running it.
+ * - Can't define animation values (duration, distance) in relation to one another: the animation
+ * XML files can't reference values in other files (you could create two dimens, but they cant really
+ * be defined in relation to one another). I ended up defining essential values (duration) in code,
+ * making the XML useless by itself.
+ * - ^ This makes starting multiple animations offset from one another difficult. You can only
+ * define startOffsets in relation to now.
+ * - Lots of trial and error to get the animation you want.
+ *
+ * Thoughts:
+ * - I'm skeptical of the value of XML files for animations coordinating multiple views:
+ *   - XML makes layouts declarative. However, XML animations can't be defined in relation to one
+ *   another, making the declarative nature less useful (e.g. values still need to be defined in code).
+ *   - XML encourages re-use, but if each animation XML is defined in relation to another, re-use is
+ *   restricted.
+ *   - You need to read multiple files in order to write one animation.
+ *
+ * Next: [ViewAnimationWithListenersActivity].
+ */
 class ViewAnimationSetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_animation_set)
 
-        /*
-        - Sucks to coordinate animations from 3 files.
-        - Can't define translation distance in relation to one another.
-        - Lots of trial/error
-        - alternative: use animation enders. TODO: write it!
-         */
         animateButton.setOnClickListener {
             it.isClickable = false // Prevent additional clicks.
 
