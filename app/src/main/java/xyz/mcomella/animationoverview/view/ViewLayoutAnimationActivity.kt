91849053f0cea592e23a58600942a1d63fff57db
Michael Comella <michael.l.comella@gmail.com>
2018-02-16 16:37:54 -0800

    Animate two views with LayoutAnimationController.

--- app/src/main/java/xyz/mcomella/animationoverview/view/ViewLayoutAnimationActivity.kt
+++ app/src/main/java/xyz/mcomella/animationoverview/view/ViewLayoutAnimationActivity.kt
@@ -15,31 +15,34 @@ import xyz.mcomella.animationoverview.R
  * A View Animation that explores the [LayoutAnimationController] API.
  *
  * Learnings:
- * - Setting a LayoutAnimationController will animate the *whole* layout
- * - You can set one animation for a ViewGroup. Presumably, you can
- *
- * I was unable to inflate in XML but didn't care to. Relpaced later?
- *
- * Grid.
- *
- * Learning
+ * - Setting a LayoutAnimationController will animate the layout of the specified ViewGroup.
+ * - This does not animate layout *changes*: it will re-animate the entire layout when views are added.
+ * There are Property Animation APIs to translate layout changes as well as transitions.
+ * - You can combine multiple ViewGroups with LayoutAnimationControllers to use different animations.
+ * - You specify only one animation XML with which to animate the views.
+ * - I was unable to inflate in the XML: the code seems brief enough that it seems unnecessary.
+ * - *THERE ARE FRAMED DROPS WHEN USING THIS FOR THE INITIAL LAYOUT ON OLDER DEVICES (GS5, N5).* You
+ * can delay the layout to fix it - see [ViewLayoutAnimationDelayedActivity].
  */
 class ViewLayoutAnimationActivity : AppCompatActivity() {
 
-    private val textSize by lazy { (textContainerView.getChildAt(1) as TextView).textSize }
+    private val textSize by lazy { (fadeContainer.getChildAt(1) as TextView).textSize }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_layout_animation)
 
         // Will animate the initial layout.
-        textContainerView.layoutAnimation = LayoutAnimationController(AnimationUtils.loadAnimation(this, android.R.anim.fade_in))
+        fadeContainer.layoutAnimation = LayoutAnimationController(AnimationUtils.loadAnimation(this, android.R.anim.fade_in))
+        translateContainer.layoutAnimation = LayoutAnimationController(AnimationUtils.loadAnimation(this, R.anim.translate_from_right))
 
         startAnimButton.setOnClickListener {
-            for (i in 0..1) {
-                val newChild = getChild()
-                textContainerView.addView(newChild, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-                textContainerView.scheduleLayoutAnimation() // Animates *THE WHOLE* layout.
+            listOf(fadeContainer, translateContainer).forEach { container ->
+                for (i in 0..1) {
+                    val fadeChild = getChild()
+                    container.addView(fadeChild, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+                }
+                container.scheduleLayoutAnimation() // Animates *THE WHOLE* layout.
             }
         }
     }
